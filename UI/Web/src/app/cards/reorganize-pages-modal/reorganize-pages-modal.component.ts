7fbaf64d49c57304bbe3b4a89aa5d805e256f9a2
Marcos Carvalho <marcosfcar@gmail.com>
2022-08-17 00:19:28 +0100

    Showing images on modal

--- UI/Web/src/app/cards/reorganize-pages-modal/reorganize-pages-modal.component.ts
+++ UI/Web/src/app/cards/reorganize-pages-modal/reorganize-pages-modal.component.ts
@@ -1,20 +1,51 @@
-import {AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Inject,
+  Input,
+  OnInit,
+  ViewChild
+} from '@angular/core';
 import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
+import {ReaderService} from '../../_services/reader.service';
+import {ChapterInfo} from '../../manga-reader/_models/chapter-info';
+import {Volume} from '../../_models/volume';
+import {Chapter} from '../../_models/chapter';
+import {ScrollService} from '../../_services/scroll.service';
+import {DOCUMENT} from '@angular/common';
 
 @Component({
   selector: 'app-reorganize-pages-modal',
   templateUrl: './reorganize-pages-modal.component.html',
   styleUrls: ['./reorganize-pages-modal.component.scss']
 })
-export class ReorganizePagesModalComponent implements OnInit, AfterViewInit {
+export class ReorganizePagesModalComponent implements OnInit, AfterViewInit, AfterContentChecked {
+
+  @ViewChild('scrollingBlock') scrollingBlock: ElementRef<HTMLDivElement> | undefined;
+  @ViewChild('companionBar') companionBar: ElementRef<HTMLDivElement> | undefined;
 
   @Input() title!: string;
 
   @ViewChild('title') inputElem!: ElementRef<HTMLInputElement>;
 
-  constructor(private modal: NgbActiveModal) { }
+  // public chapterId: number;
+  // public chapterInfo: ChapterInfo;
+  @Input() data!: Chapter;
+  pagesUrl: string[] = [];
+
+  constructor(private modal: NgbActiveModal,
+              private readerService: ReaderService,
+              private scrollService: ScrollService,
+              @Inject(DOCUMENT) private document: Document) {
+  }
 
   ngOnInit(): void {
+    //this.chapterInfo: this.readerService.getChapterInfo(this.chapterId);
+    for (let i = 1; i <= this.data.pages; i++) {
+      this.pagesUrl.push(this.readerService.getPageUrl(this.data.id, i));
+    }
   }
 
   ngAfterViewInit(): void {
@@ -22,6 +53,25 @@ export class ReorganizePagesModalComponent implements OnInit, AfterViewInit {
 
   close(): void {
     this.modal.close();
+  }
+
+  get ScrollingBlockHeight() {
+    if (this.scrollingBlock === undefined) {
+      return 'calc(var(--vh)*100)';
+    }
+    const navbar = this.document.querySelector('.navbar') as HTMLElement;
+    if (navbar === null) {
+      return 'calc(var(--vh)*100)';
+    }
+
+    const companionHeight = this.companionBar!.nativeElement.offsetHeight;
+    const navbarHeight = navbar.offsetHeight;
+    const totalHeight = companionHeight + navbarHeight + 21; //21px to account for padding
+    return 'calc(var(--vh)*100 - ' + totalHeight + 'px)';
+  }
+
+  ngAfterContentChecked(): void {
+    this.scrollService.setScrollContainer(this.scrollingBlock);
   }
 
 }
